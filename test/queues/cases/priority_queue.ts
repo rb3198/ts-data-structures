d80acfac595fe39d8f8703e52bfd7e454afc693b
rb3198 <ronitbhatia@hotmail.com>
2024-10-27 19:09:26 -0400

    Insert tests for 3-ary heaps

--- test/queues/cases/priority_queue.ts
+++ test/queues/cases/priority_queue.ts
@@ -2,6 +2,7 @@ type InsertionCase = {
   insert: [number, number];
   expectedPosition: number;
   expectedLength: number;
+  expectedQueue: [number, number][];
 };
 
 export const validBinaryPqInsertions: InsertionCase[] = [
@@ -9,32 +10,146 @@ export const validBinaryPqInsertions: InsertionCase[] = [
     insert: [5, 5],
     expectedPosition: 0,
     expectedLength: 1,
+    expectedQueue: [[5, 5]],
   },
   {
     insert: [1, 1],
     expectedPosition: 0,
     expectedLength: 2,
+    expectedQueue: [
+      [1, 1],
+      [5, 5],
+    ],
   },
   {
     insert: [2, 2],
     expectedPosition: 2,
     expectedLength: 3,
+    expectedQueue: [
+      [1, 1],
+      [5, 5],
+      [2, 2],
+    ],
   },
   {
     insert: [4, 4],
     expectedPosition: 1,
     expectedLength: 4,
+    expectedQueue: [
+      [1, 1],
+      [4, 4],
+      [2, 2],
+      [5, 5],
+    ],
   },
   {
     insert: [3, 3],
     expectedPosition: 1,
     expectedLength: 5,
+    expectedQueue: [
+      [1, 1],
+      [3, 3],
+      [2, 2],
+      [5, 5],
+      [4, 4],
+    ],
   },
 ];
 
-// 3 5 4
+export const valid3aryPqInsertions: InsertionCase[] = [
+  {
+    insert: [5, 5],
+    expectedPosition: 0,
+    expectedLength: 1,
+    expectedQueue: [[5, 5]],
+  },
+  {
+    insert: [1, 1],
+    expectedPosition: 0,
+    expectedLength: 2,
+    expectedQueue: [
+      [1, 1],
+      [5, 5],
+    ],
+  },
+  {
+    insert: [2, 2],
+    expectedPosition: 2,
+    expectedLength: 3,
+    expectedQueue: [
+      [1, 1],
+      [5, 5],
+      [2, 2],
+    ],
+  },
+  {
+    insert: [4, 4],
+    expectedPosition: 3,
+    expectedLength: 4,
+    expectedQueue: [
+      [1, 1],
+      [5, 5],
+      [2, 2],
+      [4, 4],
+    ],
+  },
+  {
+    insert: [3, 3],
+    expectedPosition: 1,
+    expectedLength: 5,
+    expectedQueue: [
+      [1, 1],
+      [3, 3],
+      [2, 2],
+      [4, 4],
+      [5, 5],
+    ],
+  },
+  {
+    insert: [8, 8],
+    expectedPosition: 5,
+    expectedLength: 6,
+    expectedQueue: [
+      [1, 1],
+      [3, 3],
+      [2, 2],
+      [4, 4],
+      [5, 5],
+      [8, 8],
+    ],
+  },
+  {
+    insert: [10, 10],
+    expectedPosition: 6,
+    expectedLength: 7,
+    expectedQueue: [
+      [1, 1],
+      [3, 3],
+      [2, 2],
+      [4, 4],
+      [5, 5],
+      [8, 8],
+      [10, 10],
+    ],
+  },
+  {
+    insert: [0, 0],
+    expectedPosition: 0,
+    expectedLength: 8,
+    expectedQueue: [
+      [0, 0],
+      [3, 3],
+      [1, 1],
+      [4, 4],
+      [5, 5],
+      [8, 8],
+      [10, 10],
+      [2, 2],
+    ],
+  },
+];
 
-export const deletions = [
+export const deletionResultsBinary = [
   [
     [2, 2],
     [3, 3],
@@ -51,5 +166,49 @@ export const deletions = [
     [5, 5],
   ],
   [[5, 5]],
+  [],
+];
+
+export const deletionResults3Ary = [
+  [
+    [1, 1],
+    [3, 3],
+    [2, 2],
+    [4, 4],
+    [5, 5],
+    [8, 8],
+    [10, 10],
+  ],
+  [
+    [2, 2],
+    [3, 3],
+    [10, 10],
+    [4, 4],
+    [5, 5],
+    [8, 8],
+  ],
+  [
+    [3, 3],
+    [5, 5],
+    [10, 10],
+    [4, 4],
+    [8, 8],
+  ],
+  [
+    [4, 4],
+    [5, 5],
+    [10, 10],
+    [8, 8],
+  ],
+  [
+    [5, 5],
+    [10, 10],
+    [8, 8],
+  ],
+  [
+    [8, 8],
+    [10, 10],
+  ],
+  [[10, 10]],
   [],
 ];
--- test/queues/priority_queue.test.ts
+++ test/queues/priority_queue.test.ts
@@ -1,4 +1,9 @@
-import { deletions, validBinaryPqInsertions } from "./cases/priority_queue";
+import {
+  deletionResults3Ary,
+  deletionResultsBinary,
+  valid3aryPqInsertions,
+  validBinaryPqInsertions,
+} from "./cases/priority_queue";
 import { PriorityQueue } from "../../src/queues";
 
 describe("Tests for pushing items into the queue -- Binary Heap", () => {
@@ -6,10 +11,12 @@ describe("Tests for pushing items into the queue -- Binary Heap", () => {
   it.each(validBinaryPqInsertions)(
     "Should return the number of elements in the queue on insertion",
     (insertion) => {
-      const { insert, expectedLength, expectedPosition } = insertion;
+      const { insert, expectedLength, expectedPosition, expectedQueue } =
+        insertion;
       const length = queue.push(insert);
       expect(length).toBe(expectedLength);
       expect(queue.at(expectedPosition)).toEqual(insert);
+      expect(queue.queue).toEqual(expectedQueue);
     }
   );
 
@@ -17,7 +24,31 @@ describe("Tests for pushing items into the queue -- Binary Heap", () => {
     let i = 0;
     while (queue.length) {
       queue.shift();
-      expect(queue).toEqual(deletions[i]);
+      expect(queue).toEqual(deletionResultsBinary[i]);
+      i++;
+    }
+  });
+});
+
+describe("Tests for pushing items into the queue -- Tri-ary Heap", () => {
+  let queue = PriorityQueue<number>(3);
+  it.each(valid3aryPqInsertions)(
+    "Should return the number of elements in the queue on insertion",
+    (insertion) => {
+      const { insert, expectedLength, expectedPosition, expectedQueue } =
+        insertion;
+      const length = queue.push(insert);
+      expect(length).toBe(expectedLength);
+      expect(queue.at(expectedPosition)).toEqual(insert);
+      expect(queue.queue).toEqual(expectedQueue);
+    }
+  );
+
+  it("Should maintain the heap property while deleting the minimum element", () => {
+    let i = 0;
+    while (queue.length) {
+      queue.shift();
+      expect(queue).toEqual(deletionResults3Ary[i]);
       i++;
     }
   });
