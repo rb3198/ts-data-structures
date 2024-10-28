c679803da98723a937c011da24f52e25df009b00
rb3198 <ronitbhatia@hotmail.com>
2024-10-27 22:12:09 -0400

    Bump to v0.0.23, type of Priority Queue fixed

--- src/queues/index.ts
+++ src/queues/index.ts
@@ -1,3 +1,3 @@
 import { PriorityQueue as PriorityQueueFactory0 } from "./priority_queue";
-export type PriorityQueue = ReturnType<typeof PriorityQueueFactory0>;
+export type PriorityQueue<T> = ReturnType<typeof PriorityQueueFactory0<T>>;
 export const PriorityQueueFactory = PriorityQueueFactory0;
