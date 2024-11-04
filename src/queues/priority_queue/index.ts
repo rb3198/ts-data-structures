4e9364f1d9f95ce5d1849cabf388b67812a83f03
rb3198 <ronitbhatia@hotmail.com>
2024-11-04 00:04:00 -0500

    1: Update Priority functionality with tests - Changed length from a property to a function reflecting the latest length of the queue - Added `degree` property to the returned queue

--- src/queues/priority_queue/index.ts
+++ src/queues/priority_queue/index.ts
@@ -42,12 +42,72 @@ export function PriorityQueue<T>(heapDegree: number = 2) {
       index = smallest;
     }
   };
+
+  /**
+   * Given an index and the updated priority, adjusts the queue to maintain its heap property, in-place.
+   * @param idx The existing index of the targeted element.
+   * @param newPriority The new priority of the element located at `idx`.
+   * @returns The new index of the given element.
+   * If an improper index is input to this function (ex: more than the length or less than -1), returns -1.
+   */
+  const updatePriority = (idx: number, newPriority: number) => {
+    if (idx >= _items.length || idx < 0) {
+      return -1;
+    }
+    // heapify up
+    let newIdx = idx;
+    _items[newIdx][1] = newPriority;
+    while (newIdx > 0) {
+      let [, priority] = _items[newIdx];
+      let parentIdx = Math.floor((newIdx - 1) / heapDegree);
+      const [, parentPriority] = _items[parentIdx];
+      if (parentPriority > priority) {
+        [_items[parentIdx], _items[newIdx]] = [
+          _items[newIdx],
+          _items[parentIdx],
+        ];
+        newIdx = parentIdx;
+      } else {
+        break;
+      }
+    }
+    if (newIdx !== idx) {
+      return newIdx;
+    }
+    // heapify down if element wasn't heapified up (priority decreased)
+    while (true) {
+      let smallestIdx = newIdx;
+      for (let k = 1; k <= heapDegree; k++) {
+        const curIdx = childIdx(newIdx, k);
+        if (curIdx >= _items.length) {
+          break;
+        }
+        const [, smallestPriority] = _items[smallestIdx];
+        const [, childPriority] = _items[curIdx];
+        if (smallestPriority > childPriority) {
+          smallestIdx = curIdx;
+        }
+      }
+      if (smallestIdx === newIdx) {
+        break;
+      }
+
+      [_items[smallestIdx], _items[newIdx]] = [
+        _items[newIdx],
+        _items[smallestIdx],
+      ];
+      newIdx = smallestIdx;
+    }
+    return newIdx;
+  };
+
   const queue = {
     push: (itemWithPriority: [T, number]) => {
       _items.push(itemWithPriority);
       heapifyUp(_items.length - 1);
       return _items.length;
     },
+    degree: heapDegree,
     min: () => {
       return _items[0];
     },
@@ -67,8 +127,10 @@ export function PriorityQueue<T>(heapDegree: number = 2) {
       }
       return minPrEl;
     },
-    length: _items.length,
+    updatePriority,
+    length: () => _items.length,
     queue: _items,
   };
+
   return queue;
 }
--- test/queues/priority_queue.test.ts
+++ test/queues/priority_queue.test.ts
@@ -1,13 +1,32 @@
 import {
   deletionResults3Ary,
   deletionResultsBinary,
+  updateBinaryPriorityTests,
+  updateTriaryPriorityTests,
   valid3aryPqInsertions,
   validBinaryPqInsertions,
 } from "./cases/priority_queue";
-import { PriorityQueue } from "../../src/queues";
+import { PriorityQueue, PriorityQueueFactory } from "../../src/queues";
 
-describe("Tests for pushing items into the queue -- Binary Heap", () => {
-  let queue = PriorityQueue<number>(2);
+const verifyHeapProperty = (queue: PriorityQueue<any>) => {
+  const { degree } = queue;
+  for (let i = 0; i < queue.length(); i++) {
+    const [, priority] = queue.queue[i];
+    for (let j = degree * i + 1; j <= degree * i + degree; j++) {
+      if (j >= queue.length()) {
+        break;
+      }
+      const [, childPriority] = queue.queue[j];
+      if (priority > childPriority) {
+        return false;
+      }
+    }
+  }
+  return true;
+};
+
+describe("Tests for pushing and deleting items into the queue -- Binary Heap", () => {
+  let queue = PriorityQueueFactory<number>(2);
   it.each(validBinaryPqInsertions)(
     "Should return the number of elements in the queue on insertion",
     (insertion) => {
@@ -22,16 +41,16 @@ describe("Tests for pushing items into the queue -- Binary Heap", () => {
 
   it("Should maintain the heap property while deleting the minimum element", () => {
     let i = 0;
-    while (queue.length) {
+    while (queue.length()) {
       queue.shift();
-      expect(queue).toEqual(deletionResultsBinary[i]);
+      expect(queue.queue).toEqual(deletionResultsBinary[i]);
       i++;
     }
   });
 });
 
-describe("Tests for pushing items into the queue -- Tri-ary Heap", () => {
-  let queue = PriorityQueue<number>(3);
+describe("Tests for pushing and deleting items into the queue -- Tri-ary Heap", () => {
+  let queue = PriorityQueueFactory<number>(3);
   it.each(valid3aryPqInsertions)(
     "Should return the number of elements in the queue on insertion",
     (insertion) => {
@@ -41,15 +60,43 @@ describe("Tests for pushing items into the queue -- Tri-ary Heap", () => {
       expect(length).toBe(expectedLength);
       expect(queue.at(expectedPosition)).toEqual(insert);
       expect(queue.queue).toEqual(expectedQueue);
+      expect(verifyHeapProperty(queue)).toBeTruthy();
     }
   );
 
   it("Should maintain the heap property while deleting the minimum element", () => {
     let i = 0;
-    while (queue.length) {
+    while (queue.length()) {
       queue.shift();
-      expect(queue).toEqual(deletionResults3Ary[i]);
+      expect(queue.queue).toEqual(deletionResults3Ary[i]);
+      expect(verifyHeapProperty(queue)).toBeTruthy();
       i++;
     }
   });
+});
+
+describe("Tests for updating the priority of an element -- Binary Heap", () => {
+  it.each(updateBinaryPriorityTests)(
+    "Should update the queue maintaining the heap property",
+    ({ queueOriginal, updateIdx, updatedPriority, queueUpdated }) => {
+      const pq = PriorityQueueFactory<number>(2);
+      queueOriginal.forEach((el) => pq.push(el));
+      pq.updatePriority(updateIdx, updatedPriority);
+      expect(verifyHeapProperty(pq)).toBeTruthy();
+      expect(pq.queue).toEqual(queueUpdated);
+    }
+  );
+});
+
+describe("Tests for updating the priority of an element -- Tri-ary Heap", () => {
+  it.each(updateTriaryPriorityTests)(
+    "Should update the queue maintaining the heap property",
+    ({ queueOriginal, updateIdx, updatedPriority, queueUpdated }) => {
+      const pq = PriorityQueueFactory<number>(3);
+      queueOriginal.forEach((el) => pq.push(el));
+      pq.updatePriority(updateIdx, updatedPriority);
+      expect(verifyHeapProperty(pq)).toBeTruthy();
+      expect(pq.queue).toEqual(queueUpdated);
+    }
+  );
 });
